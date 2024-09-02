78b64bd8eecd4f7e740ea87014ba1a48477d8381
KumariAnjali <kumarianjali206700@gmail.com>
2024-09-02 23:50:58 +0530

    files addes

---
+++ linkedlist.java/ListNode.java
@@ -0,0 +1,34 @@
+
+public class ListNode {
+         int val;
+         ListNode next;
+         //ListNode() {}
+         //ListNode(int val) { this.val = val; }
+         // ListNode(int val, ListNode next) { this.val = val; this.next = next; } 
+     }
+
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode hare = head;
+        ListNode tortoise = head;
+        ListNode lengthCheck = head;
+
+        int n =1;
+        while(lengthCheck.next != null) {
+        lengthCheck = lengthCheck.next;
+        n++;  
+        }
+        while(hare.next!=null && hare.next.next!=null){
+            hare=hare.next.next;
+            tortoise= tortoise.next;
+
+        }
+        if(n%2==0){
+            return tortoise.next;
+
+        }
+        return tortoise;
+    }
+
+    
+}
---
+++ linkedlist.java/addElements.java
@@ -0,0 +1,20 @@
+import java.util.LinkedList;
+
+public class addElements {
+
+    // Main driver method
+    public static void main(String[] args) {  
+        // Creating a LinkedList
+        LinkedList<String> list = new LinkedList<String>();
+
+        // Adding elements to the LinkedList using add() method
+        list.add("One");
+        list.add("Two");
+        list.add("Three");
+        list.add("Four");
+        list.add("Five");
+
+        // Printing the LinkedList
+        System.out.println(list);
+    }
+}   
